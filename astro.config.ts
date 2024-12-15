8001dd0031d07cc8db5bed3f11dfd54e50d22946
Nishant Joshi <mr.nishantjoshi@gmail.com>
2024-12-15 23:42:16 +0100

    minor update

--- astro.config.ts
+++ astro.config.ts
@@ -21,7 +21,7 @@ import rehypeUnwrapImages from "rehype-unwrap-images";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'nishant-codex.github.io'
+	site: 'nishant-codex.github.io',
 	image: {
 		domains: ["webmention.io"],
 	},
