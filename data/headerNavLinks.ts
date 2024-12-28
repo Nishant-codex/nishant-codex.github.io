8b86cc3d29bad66194114eb9f341656ab6347941
Nishant Joshi <mr.nishantjoshi@gmail.com>
2024-12-28 19:36:30 +0100

    added poems section

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: '/poems', title: 'Poems' },
   { href: '/about', title: 'About' },
 ]
 
