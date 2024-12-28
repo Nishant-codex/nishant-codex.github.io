3b51e2400ea873c5f7c60ceffe528bc5c1152310
Nishant Joshi <mr.nishantjoshi@gmail.com>
2024-12-28 18:57:17 +0100

    changed the layout

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export default function AuthorLayout({ children, content }: Props) {
-  const { name, avatar, occupation, company, email, twitter, bluesky, linkedin, github } = content
+  const { name, avatar, occupation, company, email, twitter, bluesky, linkedin, github, cv } = content
 
   return (
     <>
@@ -34,6 +34,7 @@ export default function AuthorLayout({ children, content }: Props) {
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
+              <SocialIcon kind="cv" href={cv} />
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
