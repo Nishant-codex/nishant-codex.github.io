39887ccead185a53e7876d02a1a5387608f2e3ee
Nishant Joshi <mr.nishantjoshi@gmail.com>
2024-12-28 19:52:13 +0100

    added cv

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -9,7 +9,7 @@ interface Props {
 }
 
 export default function AuthorLayout({ children, content }: Props) {
-  const { name, avatar, occupation, company, email, twitter, bluesky, linkedin, github, cv } = content
+  const { name, avatar, occupation, company, email, x, bluesky, linkedin, github, cv } = content
 
   return (
     <>
@@ -34,10 +34,11 @@ export default function AuthorLayout({ children, content }: Props) {
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
             <div className="flex space-x-3 pt-6">
+              <SocialIcon kind="cv" href={cv} />
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="x" href={twitter} />
+              <SocialIcon kind="x" href={x} />
               <SocialIcon kind="bluesky" href={bluesky} />
             </div>
           </div>
