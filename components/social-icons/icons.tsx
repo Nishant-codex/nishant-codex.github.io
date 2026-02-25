b7b236940f06129176260e64986bd0285e1dbd13
Nishant Joshi <mr.nishantjoshi@gmail.com>
2026-02-25 11:49:17 +0100

    Fix Prettier: remove stray semicolon in Scholar icon

--- components/social-icons/icons.tsx
+++ components/social-icons/icons.tsx
@@ -65,6 +65,7 @@ export function X(svgProps: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
 export function Scholar(svgProps: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...svgProps}>
@@ -74,8 +75,9 @@ export function Scholar(svgProps: SVGProps<SVGSVGElement>) {
         d="M12 24a7 7 0 1 1 0-14 7 7 0 0 1 0 14zm0-24L0 9.5l4.838 3.94A8 8 0 0 1 12 9a8 8 0 0 1 7.162 4.44L24 9.5z"
       />
     </svg>
-  );
+  )
 }
+
 export function Youtube(svgProps: SVGProps<SVGSVGElement>) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...svgProps}>
