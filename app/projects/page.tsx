5a78438f19714c4914eee911aaccb75c17ff3b80
Nishant Joshi <mr.nishantjoshi@gmail.com>
2026-06-26 21:43:41 +0530

    minor changes

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-          These are some of the projects I have taken up during my career. 
+            These are some of the projects I have taken up during my career.
           </p>
         </div>
         <div className="container py-12">
